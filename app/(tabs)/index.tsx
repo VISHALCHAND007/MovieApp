b27aec1f4fa9796fc1926615468cec0fe65a77f6
Vishal Chand <sam.chand96@gmail.com>
2025-03-19 22:14:03 +0530

    Done with the routes and created a custom navigation bar.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className={"font-bold text-dark-200 text-3xl"}>Hello World!</Text>
+    </View>
+  );
+}
