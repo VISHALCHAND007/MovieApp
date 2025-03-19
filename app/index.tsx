49c94ab25955665a330e9ab1216f75777bd10a76
Vishal Chand <sam.chand96@gmail.com>
2025-03-19 13:52:45 +0530

    Added the colors used throughout the app and complete the native wind setup.

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,8 @@ import {Text, View} from "react-native";
 
 export default function Index() {
     return (
-        <View
-            className="flex-1 justify-center items-center"
-        >
-            <Text className="font-bold text-blue-500 text-5xl">Welcome home!</Text>
+        <View className="flex-1 justify-center items-center">
+            <Text className={"font-bold text-dark-200 text-3xl"}>Hello World!</Text>
         </View>
     );
 }
